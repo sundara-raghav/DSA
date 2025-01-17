124eea755fc871feffd906537de3c8423b8dee30
Sundara Raghav.G <g.sundararaghav2005@gmail.com>
2025-01-17 11:38:06 +0530

    Delete python/Py_training directory

--- python/Py_training/Duplicate.py
+++
@@ -1,9 +0,0 @@
-def containsDuplicate(nums):
-    nums.sort()
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] == nums[j]:  
-                return True
-    return False
-num = list(map(int, input("Enter numbers: ").split()))
-print(containsDuplicate(num))
--- python/Py_training/palindrome.py
+++
@@ -1,9 +0,0 @@
-def palindrome(n):
-    if n == n[::-1]: 
-        return True
-    else:
-        return False
-
-num = input("Enter string: ")
-print(palindrome(num))
-  
